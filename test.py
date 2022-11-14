ac4eb52897f198057796e55e11b1a525fdc3b98e
kojunseo <sta06167@naver.com>
2022-11-14 19:36:42 +0900

    1.2.5: Final Error

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-from dotshow.experiment.movie import movshow
-from dotshow.matching import __stdshow__
-from sys import stdout
-import numpy as np
-from PIL import Image
+from dotshow import loadshow
+
+loadshow("school.jpg")
 
 
-__stdshow__(np.array(Image.open("대표최성.png")), gray=False, size=5)
+# __stdshow__(np.array(Image.open("대표최성.png")), gray=False, size=5)
 
 # movshow("hello.mov", gray=True, size=5, limit=5)
